152ca93ae5770a81f25349f9d7b82681ac2ce38a
morkeeth <42701407+Morkeeth@users.noreply.github.com>
2021-12-26 21:25:10 +0100

    App update

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+const CONTRACT_ADDRESS = '0x3f7BbcF808900e37E6ac891C0FBfe450F2453010';
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
